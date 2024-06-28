7da2d6e913bf0b89ab8cdaf011e5ba2eb8f05982
akshch <akshaych2786@gmail.com>
2024-06-28 10:06:30 +0530

    JWT integration

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+
+      post '/auth/login', to: 'authentication#login'
+      get '/*a', to: 'application#not_found'
     end
   end
 end
